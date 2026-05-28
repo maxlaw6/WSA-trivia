36fe6080c18abb95db1bba5874c2f6410ea7f22b
maxlaw6 <167712338+maxlaw6@users.noreply.github.com>
2026-05-27 21:13:32 -0600

    Implement game start functionality in lobby

--- src/app/host/game/[id]/lobby.tsx
+++ src/app/host/game/[id]/lobby.tsx
@@ -1,4 +1,64 @@
+'use client'
+
 import { Participant, supabase } from '@/types/types'
+import { useQRCode } from 'next-qrcode'
+
+export default function Lobby({
+  participants: participants,
+  gameId,
+}: {
+  participants: Participant[]
+  gameId: string
+}) {
+  const { Canvas } = useQRCode()
+
+  const onClickStartGame = async () => {
+    const { data, error } = await supabase
+      .from('games')
+      .update({ phase: 'quiz' })
+      .eq('id', gameId)
+    if (error) {
+      return alert(error.message)
+    }
+  }
+
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="flex justify-between m-auto bg-black p-12">
+        <div className="w-96">
+          <div className="flex justify-start flex-wrap pb-4">
+            {participants.map((participant) => (
+              <div
+                className="text-xl m-2 p-2 bg-green-500"
+                key={participant.id}
+              >
+                {participant.nickname}
+              </div>
+            ))}
+          </div>
+
+          <button
+            className="mx-auto bg-white py-4 px-12 block text-black font-bold uppercase tracking-wider"
+            onClick={onClickStartGame}
+          >
+            Start Game
+          </button>
+        </div>
+        <div className="pl-4">
+          <Canvas
+            text={`https://wsa-trivia.vercel.app/game/${gameId}`}
+            options={{
+              errorCorrectionLevel: 'M',
+              margin: 3,
+              scale: 4,
+              width: 400,
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  )
+}import { Participant, supabase } from '@/types/types'
 import { useQRCode } from 'next-qrcode'
 
 export default function Lobby({
