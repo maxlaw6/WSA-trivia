2ff08b765b7dd5b7acbbb4f98109b1a6f4ed0cda
maxlaw6 <167712338+maxlaw6@users.noreply.github.com>
2026-05-28 09:27:21 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,6 @@
-import createMDX from '@next/mdx'
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/host/dashboard',
-        permanent: true,
-      },
-      {
-        source: '/host',
-        destination: '/host/dashboard',
-        permanent: true,
-      },
-    ]
-  },
-}
+  /* any other settings can stay here, just remove the redirects function entirely */
+};
 
-const withMDX = createMDX({
-  // Add markdown plugins here, as desired
-})
-
-export default withMDX(nextConfig)
+export default nextConfig;
